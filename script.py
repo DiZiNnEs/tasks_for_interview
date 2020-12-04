3e37536e2b48139660c852e6ee35614896555b82
Iglek Kuat <dizinnes228@gmail.com>
2020-12-04 22:41:47 +0600

    feat: add update

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 from math import prod
 
 
-def count_multipliers(*args) -> prod:
+def count_multipliers(*args) -> int:
     return prod(args)
--- test_script.py
+++ test_script.py
@@ -8,6 +8,7 @@ class TestScript(unittest.TestCase):
         self.assertEqual(script.count_multipliers(2, 2), 4)
         self.assertEqual(script.count_multipliers(15, 23, 432), 149040)
         self.assertEqual(script.count_multipliers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 3628800)
+        self.assertEqual(script.count_multipliers(2, 0), 0)
 
 
 if __name__ == '__main__':
