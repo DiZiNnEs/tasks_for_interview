d5bd3050a9a8d692579f6ea37ec36384ff2171f6
Iglek Kuat <dizinnes228@gmail.com>
2020-12-04 22:51:37 +0600

    refactor: update type

--- script.py
+++ script.py
@@ -3,22 +3,22 @@ from json import load, dumps
 from nested_diff import diff
 
 
-def read_json_file(path: str) -> load:
+def read_json_file(path: str) -> dict:
     with open(path) as json_file:
         return load(json_file)
 
 
-def get_different_between_two_files(first_json_file: load, second_json_file: load) -> diff:
+def get_different_between_two_files(first_json_file: load, second_json_file: load) -> dict:
     different = diff(first_json_file, second_json_file)
     return different
 
 
-def get_json_human_readable(first_json_file: load, second_json_file: load):
+def get_json_human_readable(first_json_file: load, second_json_file: load) -> str:
     different = diff(first_json_file, second_json_file)
     return dumps(different, indent=2)
 
 
-a = read_json_file('first_data.json')
-b = read_json_file('second_data.json')
-
-print(get_json_human_readable(a, b))
+# a = read_json_file('first_data.json')
+# b = read_json_file('second_data.json')
+#
+# print(get_json_human_readable(a, b))
